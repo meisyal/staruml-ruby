5d6711c891d3c3256a43f62cfe46ecc09fdcff3d
Andrias Meisyal <andriasonline@gmail.com>
2017-10-31 22:38:47 +0700

    Add unit test

---
+++ unit-test.js
@@ -0,0 +1,17 @@
+define(function (require, exports, module) {
+  'use strict';
+
+  var Repository,
+      ProjectManager,
+      CommandManager,
+      Dialogs,
+      FileSystem = app.getModule('filesystem/FileSystem'),
+      SpecRunnerUtils = app.getModule('spec/SpecRunnerUtils'),
+      FileUtils = app.getModule('file/FileUtils'),
+      ExtensionUtils = app.getModule('utils/ExtensionUtils'),
+      UML = app.getModule('uml/UML');
+
+  describe('Ruby', function () {
+    // TODO: Test Case for Parsing
+  })
+})
