498f8d5e93c45be09a95871b9edc79ceb1e6966b
Andrias Meisyal <andriasonline@gmail.com>
2021-01-18 22:14:49 +0700

    Update year

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 Andrias Meisyal. All rights reserved.
+ * Copyright (c) 2016-2021 Andrias Meisyal. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
